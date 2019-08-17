c0340c7dbc87ece2388c4f194f6212f163c354e2
Jitendra <jitendra.saroj93@gmail.com>
2019-08-17 21:13:57 +0530

    initial project setup for todo app

---
+++ build/precache-manifest.e92bd5d99e40148c2c58de08eff00335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87c77f80f53262d5a29",
+    "url": "/static/css/main.f7cdb123.chunk.css"
+  },
+  {
+    "revision": "c87c77f80f53262d5a29",
+    "url": "/static/js/main.c87c77f8.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a48ce32fc41bfe52634641095e3be5ea",
+    "url": "/index.html"
+  }
+];
